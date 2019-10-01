dc887c9785cd55955e7c758145e5e480fca8b9db
Gordon Watts <gordon@gordonandpaula.net>
2019-10-01 10:18:42 +0200

    FIrst import
    With working tests!

---
+++ tests/test_util_LINQ.py
@@ -0,0 +1,42 @@
+# A few tests on the LINQ ast funcitonality
+from func_adl import EventDataset
+from func_adl.xAOD.backend.util_LINQ import find_dataset
+import ast
+
+
+def test_find_EventDataSet_good():
+    a = EventDataset("file://junk.root") \
+        .value(executor=lambda a: a)
+
+    assert ["file:///junk.root"] == find_dataset(a).url
+
+def test_find_EventDataSet_none():
+    a = ast.parse("a+b*2")
+
+    try:
+        find_dataset(a)
+        assert False
+    except:
+        pass
+
+def test_find_EventDataset_Select():
+    a = EventDataset("file://dude.root") \
+        .Select("lambda x: x") \
+        .value(executor=lambda a: a)
+
+    assert ["file:///dude.root"] == find_dataset(a).url
+
+def test_find_EventDataset_SelectMany():
+    a = EventDataset("file://dude.root") \
+        .SelectMany("lambda x: x") \
+        .value(executor=lambda a: a)
+
+    assert ["file:///dude.root"] == find_dataset(a).url
+
+def test_find_EventDataset_Select_and_Many():
+    a = EventDataset("file://dude.root") \
+        .Select("lambda x: x") \
+        .SelectMany("lambda x: x") \
+        .value(executor=lambda a: a)
+
+    assert ["file:///dude.root"] == find_dataset(a).url
